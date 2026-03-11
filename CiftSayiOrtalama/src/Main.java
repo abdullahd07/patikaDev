f80adb81cc9c9b49fd51b8135659a499b626ffbb
Abdullah <abdullahdegirmenci07@gmail.com>
2026-03-11 23:16:35 +0300

    Girilen değere kadar olan sayıların ortalamasını alma

---
+++ CiftSayiOrtalama/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Sayı Giriniz...");
+        int a = scanner.nextInt();
+        int toplam =0, uygunSayi =0;
+        double sonuc;
+
+
+        for  (int i = 1; i <= a; i++) {
+            if (i%12 == 0) {
+                toplam += i;
+                uygunSayi++;
+            }
+        }
+
+        sonuc = toplam / uygunSayi;
+
+        System.out.println("Girmiş olduğunuz" + a + " değerine kadar 3'e ve 4'e bölünebilen sayıların ortalaması: " + sonuc);
+
+
+    }
+}
