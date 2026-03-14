36b59cc6c508046f6e6f4eedde86c6a15bdd60a0
Abdullah <abdullahdegirmenci07@gmail.com>
2026-03-14 11:41:34 +0300

    Harmonik Sayı Hesaplama First Commit

---
+++ HarmonikSeri/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı Giriniz:");
+        int number = scan.nextInt();
+        double result = 0;
+        for (double i = 1; i <= number; i++) {
+            result += (1/ i);
+        }
+        System.out.println(number + " için Harmonik Seri değeri: "+ result +" olarak hesaplanmaktadır.");
+    }
+}
