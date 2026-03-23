156252cea3b29707d8fb53047ac70b3044d96c1f
Abdullah <abdullahdegirmenci07@gmail.com>
2026-03-23 14:46:02 +0300

    Seri sayısı için kullanıcı giriş yapısı eklendi. ReadMe file eklendi

--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int number=6, a =0,b=1,c=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ("Fibonacci Serisi için basamak sayısı giriniz: ");
+        int number=scanner.nextInt();
+        int a =0,b=1,c=0;
 
-        for(int i =0; i<=number; i++){
+        for(int i =0; i<number; i++){
             c= a + b;
             System.out.println(a + " + " + b +" = " +c);
             a=b;
